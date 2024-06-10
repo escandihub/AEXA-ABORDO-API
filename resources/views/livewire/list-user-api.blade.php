52c38a5325707f5951346daa3ec677017ec0be48
Emmanuel Espinoza <eespinest87@gmail.com>
2024-06-10 17:21:15 -0600

    upt: se agrega colores de la tabla

--- resources/views/livewire/list-user-api.blade.php
+++ resources/views/livewire/list-user-api.blade.php
@@ -1,7 +1,7 @@
 <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
     <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-            <tr>
+        <thead class="text-xs text-gray-700 uppercase bg-blue-700 rounded-sm  dark:bg-gray-700 dark:text-gray-400">
+            <tr class="rounded-sm">
                 <th scope="col" class="px-6 py-3">
                     Nombre
                 </th>
@@ -15,8 +15,6 @@
         </thead>
         <tbody>
             @foreach ($users as $user)
-
-            @endforeach
             <tr
                 class="odd:bg-black odd:dark:bg-gray-900 even:bg-blue-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
                 <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
@@ -29,6 +27,7 @@
                     <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
                 </td>
             </tr>
+            @endforeach
         </tbody>
     </table>
 </div>
