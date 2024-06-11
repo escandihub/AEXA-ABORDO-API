c01ceecb853973b52344c5be48d299febb60343c
Emmanuel Espinoza <eespinest87@gmail.com>
2024-06-10 18:59:51 -0600

    upt: api auth token

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,14 @@
 <?php
 
+use App\Http\Controllers\ClienController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\RegisterClientAPI;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+Route::post('register',[ ClienController::class, 'register'])->middleware(RegisterClientAPI::class);
+Route::post('login',[ ClienController::class, 'createToken']);
