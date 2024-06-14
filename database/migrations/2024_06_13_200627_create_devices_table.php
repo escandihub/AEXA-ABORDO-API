f014e16feaed323c9c271e89c4457b9b6b41f062
Emmanuel Espinoza <eespinest87@gmail.com>
2024-06-13 18:58:24 -0600

    upt: seguimiento de dispositivos de los accesos de disoisitivo

---
+++ database/migrations/2024_06_13_200627_create_devices_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('devices', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('usuario_id');
+            $table->string('model');
+            $table->string('plataform');
+            $table->string('operatingSystem');
+            $table->string('osVersion');
+            $table->boolean('isVirtual');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('devices');
+    }
+};
