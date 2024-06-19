81177a96a21957cffbfe4161f7bc429473c8ac9b
Alejandro Escandon <aleskndon@gmai.com>
2024-06-19 14:56:14 -0600

    r

--- app/Http/Controllers/CorridasController.php
+++ app/Http/Controllers/CorridasController.php
@@ -57,7 +57,6 @@ class CorridasController extends Controller
             "route" => [
                 "from" => $diario->origen,
                 "to" => $diario->destino,
-                "to2" => $diario->destino,
             ],
             "pasajeros" => PasajeroResource::collection($v)->resolve()
 
