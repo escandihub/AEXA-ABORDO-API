07572444e88f2b3ef90afed7c28c2c06957051ff
Alejandro Escandon <aleskndon@outlook.com>
2024-06-26 12:59:50 -0600

    upt: se guarda datos de la latitud y longitud

--- app/Http/Controllers/ClienController.php
+++ app/Http/Controllers/ClienController.php
@@ -30,7 +30,8 @@ class ClienController extends Controller
         $user = Usuario::where('user', $request->user)->first();
         return response()->json([
             "user" => $user,
-            "token" => $user->createToken('omnibus', ["*"], now()->addDay())->plainTextToken
+            "token" => $user->createToken('omnibus', ["*"], now()->addMinute(1), $request->lat, $request->log)->plainTextToken,
+            
         ], 200);
 
     }
