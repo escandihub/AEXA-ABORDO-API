108e06bf864d7f88e16d07f50621594487c83a9d
Alejandro Escandon <aleskndon@outlook.com>
2024-06-28 19:03:20 -0600

    upt; mejoras de consulta

--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Middleware\RegisterClientAPI;
 use App\Http\Middleware\LoginUser;
 use App\Http\Middleware\isPassengerValid;
+use App\Http\Middleware\VadilateLocation;
 use Illuminate\Routing\RouteGroup;
 use App\Http\Controllers\PasajerosController;
 
@@ -16,8 +17,8 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::middleware(['auth:sanctum'])->group(function () {
-    Route::get('corridas',[ CorridasController::class, 'index']); 
-    Route::get('pasajeros/{id}',[ CorridasController::class, 'show']); 
+    Route::get('corridas',[ CorridasController::class, 'index']); // ->middleware(VadilateLocation::class); 
+    Route::get('pasajeros/{id}',[ CorridasController::class, 'show']); //->middleware(VadilateLocation::class); 
     Route::put('pasajero/{pasajero_id}',[ CorridasController::class, 'update'])->middleware(isPassengerValid::class); 
     Route::get('pasajero/asientos/{corrida}',[ PasajerosController::class, 'asientos']); 
 });
