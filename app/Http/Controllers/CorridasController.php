bbad6d6e40a51afe24233f052dfebd60e208a096
Alejandro Escandon <aleskndon@outlook.com>
2024-10-12 14:24:28 -0600

    fix: hora se reduce los horarios con filtro en madrugada

--- app/Http/Controllers/CorridasController.php
+++ app/Http/Controllers/CorridasController.php
@@ -276,26 +276,34 @@ class CorridasController extends Controller
     
         $this->madrugada = false;
 
-        $hoy = \Carbon\CarbonImmutable::parse('2024-10-10 07:00');  # \Carbon\CarbonImmutable::now(); #\Carbon\CarbonImmutable::parse('2024-08-20 01:00'); 
+        $hoy = \Carbon\CarbonImmutable::parse('2024-10-11 12:30');  # \Carbon\CarbonImmutable::now(); #\Carbon\CarbonImmutable::parse('2024-08-20 01:00'); 
         // $fecha2 = \Carbon\Carbon::parse('2024-08-19 23:30')->addHour(2);
         $fecha2 = $hoy->copy()->addHour(3);
 
         // $fecha = $hoy->format('Y-m-d');
-        $fecha = $hoy->subHour(2);
+        $fecha = $hoy->subHour(6);
         // SI EL HORARIO ACTUAL ES DE MADRUGADA REALIZAR UNA CONSULTA EL DIA ANTERIOR A LAS 23 HORAS
         if($hoy->format('H') >= 00 &&  $hoy->format('H') <= 3){
             \Log::info('es de madrugada?');
-            // $this->madrugada = true; 
+            $this->madrugada = true; 
             $fecha = $hoy->subDay(1)->setTime(21, 30, 00);
         }
 
         $columnas = [];
         $result = '';
-        $terminal_user =  "TON terminal"; //$request->user()->empleado->nombre_taquilla; #terminal->abreviacion;  #nombre_taquilla;
+        $terminal_user =  $request->user()->empleado->nombre_taquilla; //$request->user()->empleado->nombre_taquilla; #terminal->abreviacion;  #nombre_taquilla;
         // \Log::info("fechas --- inter");
         //  \Log::info($fecha);
         //  \Log::info($fecha2);
         // \Log::info($terminal_user);
+
+
+        $corridas = DB::table('diario_c')->select('*') //->where('fecha', '=', $fecha)
+        ->whereRaw('STR_TO_DATE(CONCAT(fecha," ", hora,":",minutos), "%Y-%m-%d %H:%i") between ? AND ?', [$fecha->format('Y-m-d H:i'), $fecha2->format('Y-m-d H:i')])
+        ->where('condicion_corrida','=', 'Disponible')
+        // ->orderBy('hora', 'asc')
+        ->get(); 
+/*
         if ($hoy->format('H') >= 23 || $hoy->format('H') <= 2) {
         $corida_hora = DB::table('diario_c')->select('id_diario_c')->whereRaw('(fecha = ? and hora >= ?) or (fecha = ? and hora <= ?)', [$fecha->format("Y-m-d"), $fecha->format("H"), $fecha2->format("Y-m-d"), $fecha2->format("H")])->get();
            
@@ -308,17 +316,21 @@ class CorridasController extends Controller
         ->get(); 
         }else{
             $corridas = DB::table('diario_c')->select('*') //->where('fecha', '=', $fecha)
-        ->whereRaw('STR_TO_DATE(CONCAT(fecha), "%Y-%m-%d") between ? AND ?', [$fecha->format('Y-m-d'), $fecha2->format('Y-m-d')])
+        ->whereRaw('STR_TO_DATE(CONCAT(fecha), "%Y-%m-%d %H:%i") between ? AND ?', [$fecha->format('Y-m-d'), $fecha2->format('Y-m-d')])
         ->where('condicion_corrida','=', 'Disponible')
         // ->orderBy('hora', 'asc')
         ->get(); 
-        }
+        } */
         //  \Log::info($corridas);
         /**
          * Se hace el recorido de las corridas para obtener las columnas correspondientes
          * al usuario actual autenticado mediante un procedimiento almacenado.
-         */
-
+        //  */
+        // \Log::info("--------");
+        // \Log::info("inicia: {$fecha->format('Y-m-d H:i')} - {$fecha2->format('Y-m-d H:i')} ") ;
+        //  \Log::info('primer select');
+        //  \Log::info($corridas);
+        //  \Log::info("--------");
         foreach ($corridas as $key => $corrida) {                                                  # TERMINAL  quite esa frase
             $result =   DB::select("call getColumn(?,?,@val)", [$corrida->id_diario_c, "{$terminal_user}"]);
 
@@ -339,7 +351,7 @@ class CorridasController extends Controller
         \Log::info($finH);
         $filtroDate = $this->lastFilter($corridas_, $hoy, $fecha2);
         $filtroDate->values();
-        $diario = DB::table('diario_c')->selectRaw('diario_c.id_diario_c,diario_c.origen,diario_c.destino,diario_c.clase,diario_c.autobus,diario_c.capacidad,diario_c.disponibles,diario_c.fecha, diario_c.hora, diario_c.minutos')->whereIn('id_diario_c', $filtroDate->pluck('id_diario_c'))->orderBy("fecha", "asc")->get()
+        $diario = DB::table('diario_c')->selectRaw('diario_c.id_diario_c,diario_c.origen,diario_c.destino,diario_c.clase,diario_c.autobus,diario_c.capacidad,diario_c.disponibles,diario_c.fecha, diario_c.hora, diario_c.minutos')->whereIn('id_diario_c', $filtroDate->pluck('id_diario_c'))->orderBy("fecha", "desc")->get()
         ->map(function($corrida)use($filtroDate){
             $terminal = $filtroDate->filter(function($terminal)use($corrida){ return $terminal->id_diario_c == $corrida->id_diario_c; });
             return [
@@ -391,7 +403,7 @@ class CorridasController extends Controller
             #agregar filtro despues de hacer pruebas prod
             // && $c->status != 'S'
             // \Log::info($date_corrida);
-             return $this->comparative($date_corrida, $fecha, $fecha2)  &&  $c->status != 'C' && $c->status != 'F';
+             return $this->comparative($date_corrida, $fecha, $fecha2)  &&  $c->status != 'C' && $c->status != 'F' &&  $c->status != 'S' ;
             // return  $date_corrida->between($fecha, $fecha2)  &&  $c->status != 'C' && $c->status != 'F';
             // if($this->madrugada){
             //     return  $c->status != 'C' && $c->status != 'F';
@@ -405,21 +417,39 @@ class CorridasController extends Controller
         // $currentTime = new \DateTime($date->format('Y-m-d H:i'));
         // $startTime = new \DateTime($fecha1->format('Y-m-d H:i'));
         // $endTime = new \DateTime($fecha2->format('Y-m-d H:i'));
-        $currentTime = $date->format("H:i");
-        $startTime = $fecha1->subHour(1)->format("H:i");
-        $endTime =  $fecha1->addHour(1)->format("H:i");
+        $currentTime = $date;
+        $startTime = $fecha1->copy()->subMinutes(30);
+        $endTime =  $fecha1->copy()->addMinutes(30);
         //\Log::info('hoy: ' . $currentTime->format('Y-m-d H:i') . ">=" . 'start: ' .  $startTime->format('Y-m-d H:i') . "<=" . "end: " . $endTime->format('Y-m-d H:i'));
         // \Log::info('- comparativa -');
         // \Log::info($currentTime->format('H:i') . ">=" . $startTime->format('H:i') . "<=" . $endTime->format('H:i'));
         if($this->madrugada){
-            if ($currentTime >= $startTime  && $currentTime <= $endTime) {
+            \Log::info('hoy: ' . $currentTime->format('Y-m-d H:i') . ">=" . 'start: ' .  $fecha2->format('Y-m-d H:i') . ' || ' . 'hoy: ' . $currentTime->format('Y-m-d H:i') . "<=" . "end: " . $endTime->format('Y-m-d H:i'));
+        
+           // $currentTime = $date->format("Y-m-d  H:i");
+           // $startTime = $fecha2->format("Y-m-d H:i");
+            //$endTime =  $fecha1->format("Y-m-d H:i");
+            // if ($currentTime->between($fecha2, $endTime)) {
+            //\Log::info('hoy: ' . $currentTime->format('Y-m-d H:i') . ">=" . 'start: ' .  $startTime->format('Y-m-d H:i') . "<=" . "end: " . $endTime->format('Y-m-d H:i'));
+            //if ($fecha2->format("H") >= $currentTime->format("H") && $currentTime->format("H") <= $endTime->format("H")) { // forma que ya queda solo mustra unas mas pero solo en madrugada
+            $currentTime = $this->convertDate($date, $endTime);
+            if ($currentTime->between($startTime, $endTime)) {
                 return true;
             }
         }else{
-            if ($currentTime >= $startTime  && $currentTime <= $endTime) {
+            if ($currentTime->between($startTime, $endTime)) {
+            // if ($currentTime >= $startTime  && $currentTime <= $endTime) {
                 return true;
             }
         }
         return false;
+    }
+
+    function convertDate($date, $end){
+        \Log::info('---- : ' .  $date->format('Y-m-d H:i'));
+        if ($date->format("H") >= 0 && $date->format("H") <= 3 && $date->format("d") != $end->format("d")) {
+            return $date->addDay(1);
+        }
+        return $date;
     }
 }
