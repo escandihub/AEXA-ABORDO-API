07aa8ff9e32b345b8fa76ba2799be48ed4e721e3
Alejandro Escandon <aleskndon@outlook.com>
2024-10-12 14:25:46 -0600

    upt: fecha de hoy now

--- app/Http/Controllers/CorridasController.php
+++ app/Http/Controllers/CorridasController.php
@@ -276,7 +276,7 @@ class CorridasController extends Controller
     
         $this->madrugada = false;
 
-        $hoy = \Carbon\CarbonImmutable::parse('2024-10-11 12:30');  # \Carbon\CarbonImmutable::now(); #\Carbon\CarbonImmutable::parse('2024-08-20 01:00'); 
+        $hoy = \Carbon\CarbonImmutable::now();  # \Carbon\CarbonImmutable::now(); #\Carbon\CarbonImmutable::parse('2024-08-20 01:00'); 
         // $fecha2 = \Carbon\Carbon::parse('2024-08-19 23:30')->addHour(2);
         $fecha2 = $hoy->copy()->addHour(3);
 
