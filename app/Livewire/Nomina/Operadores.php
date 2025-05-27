f3c4a4f1925bfc4e03c52697eb38cc8e8df3e0a7
Alejandro Escandon <aleskndon@outlook.com>
2025-05-27 10:34:28 -0600

    upt: se agrega filtro para manejar los datos que se muestran en la tabla

--- app/Livewire/Nomina/Operadores.php
+++ app/Livewire/Nomina/Operadores.php
@@ -3,6 +3,11 @@
 namespace App\Livewire\Nomina;
 
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Builder;
+
+use Livewire\WithoutUrlPagination;
+use Livewire\WithPagination;
+use Livewire\Attributes\Url;
 
 use Livewire\Component;
 use App\Exports\CorridasPorConductorSheet;
@@ -10,11 +15,18 @@ use App\Exports\OperadoresExport;
 
 class Operadores extends Component
 {
+    use WithPagination, WithoutUrlPagination;
+
+    #[Url]
+    public ?string $filter = 'all';
+    #[Url]
+    public ?string $search = '';
+
     public function render()
     {
         // $this->query();
         return view('livewire.nomina.operadores', [
-            'corridas' =>  $this->query(),
+            'corridas' =>  $this->filters()->paginate(10),
             'operadores' => [
                 [
                     'id' => 1,
@@ -62,8 +74,7 @@ class Operadores extends Component
         $now = \Carbon\CarbonImmutable::now();
         $corrida = DB::table('diario_c')->whereBetween('fecha', ["2025-05-20", $now->format('Y-m-d')])->where('condicion_corrida', 'Disponible')
             ->select('fecha', 'hora', 'minutos', 'origen', 'destino', 'autobus', 'clase', 'operador1', 'operador2', 'id_diario_c')
-            ->orderBy('fecha')
-            ->get();
+            ->orderBy('fecha');
 
         // \Log::info($corrida);
         return $corrida;
@@ -72,6 +83,24 @@ class Operadores extends Component
         WHERE fecha between '2025-04-16' and '2025-04-30' and condicion_corrida = "Disponible"
         ORDER BY fecha DESC, operador1 DESC
          */
+    }
+
+    private function filters()
+    {
+        $now = \Carbon\CarbonImmutable::now();
+        \Log::info($now->subMinutes(30)->format('H') . ':' . $now->subMinutes(30)->format('i'));
+        \Log::info($now->addMinutes(30)->format('H') . ':' . $now->addMinutes(30)->format('i'));
+
+       return $this->query()->when($this->filter == 'now', function ($query) use ($now) {
+            $query
+            ->whereBetween('hora', [$now->subMinutes(30)->format('H'), $now->addMinutes(30)->format('H')]);
+            // ->whereBetween('minutos', [$now->subMinutes(30)->format('i'), $now->addMinutes(30)->format('i')]);
+        })->when($this->search, function ($query) {
+            $query->where(function ($q) {
+                $q->where('autobus', 'like', '%' . $this->search . '%')
+                    ->orWhere('operador1', 'like', '%' . $this->search . '%');
+            });
+        });
     }
 
     /**
@@ -85,12 +114,13 @@ ORDER BY hora ASC
 
 select * from `sessions` where `id` = "M58i05QvTZDQsuTAhgtMrCunORQvsCJxfWakJCA2" limit 1
      */
-    public function generateReport(){
+    public function generateReport()
+    {
         $datos = $this->query();
-        $Formating = new CorridasPorConductorSheet($datos);
+        $Formating = new CorridasPorConductorSheet($datos->get());
         // dd($datos);
         $array = $Formating->array();
-        
+
         return \Excel::download(new OperadoresExport($array, [$Formating->header, $Formating->subheader]), 'operadores.xlsx');
     }
 }
