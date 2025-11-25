df4b1abc11ce0baf17ec27a732de6e485bb5e6e6
Alejandro Escandon <aleskndon@outlook.com>
2025-11-25 14:08:06 -0600

    upt:

--- database/migrations/2025_10_17_122237_create_api_tokens_table.php
+++ database/migrations/2025_10_17_122237_create_api_tokens_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('api_tokens', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id');
             $table->string('token', 64)->unique();
             $table->boolean('active')->default(true);
             $table->timestamps();
