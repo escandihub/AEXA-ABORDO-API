95db657291cd974f5333de7cae127253386d9b96
Alejandro Escandon <aleskndon@outlook.com>
2025-11-26 17:51:41 -0600

    upt: consumo de recurso de operadores internos

--- app/Http/Controllers/ApiOperadores/OperadoresController.php
+++ app/Http/Controllers/ApiOperadores/OperadoresController.php
@@ -5,11 +5,14 @@ namespace App\Http\Controllers\ApiOperadores;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\ExternalOperador as ModelsExternalOperador;
-
+use App\Http\Controllers\ApiOperadores\OperadoresService;
 class OperadoresController extends Controller
 {
     public function index(Request $request)
     {
+        $service = app(OperadoresService::class);
+        $aexaOperadores = $service->getOperadoresAexaTours();
+
         $query = ModelsExternalOperador::query();
         if ($request->has('marca')) {
             $query->where('Empresa', 'LIKE', "%{$request->marca}%");
@@ -20,7 +23,8 @@ class OperadoresController extends Controller
         }
 
         if ($request->has('todo') && $request->todo === 'true') {
-            return response()->json($query->get());
+            $operadores = $query->get();
+            return response()->json($service->mergeCollections($aexaOperadores, $operadores));
         }
 
         // Paginado por defecto
