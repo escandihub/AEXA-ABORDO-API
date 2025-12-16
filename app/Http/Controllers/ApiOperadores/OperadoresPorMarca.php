d424adc93eb6fbb703a7fe35f2cab5945af3e32e
Alejandro Escandon <aleskndon@outlook.com>
2025-12-16 14:42:18 -0600

    upt: filtro invertido

--- app/Http/Controllers/ApiOperadores/OperadoresPorMarca.php
+++ app/Http/Controllers/ApiOperadores/OperadoresPorMarca.php
@@ -14,10 +14,10 @@ class OperadoresPorMarca
 
     public function enumMarca($marca, Builder $query ){
         if($marca == 'AE'){
-            return $query->whereNotIn('Empresa', ["MEXIC","TRANS"])
+            return $query->whereIn('Empresa', ["AEXPR","TRANS"])
             ->where(function ($query) { return $query->where('Puesto', 'LIKE', '%PERADOR%'); });
         }else if($marca == 'TI'){ // titanium
-            return $query->Where('Empresa', 'TRANS')->OrWhere('Empresa', 'MEXIC')
+            return $query->WhereIn('Empresa', ['MEXIC', 'TITAN'])
             ->where(function ($query) { return $query->where('Puesto', 'LIKE', '%PERADOR%'); });
         }else{
              throw new \Exception('non supported indicator');
